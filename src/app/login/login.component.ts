2d5d4a055bf118b82baf6c371335d5784e307d9e
Suhejb <suhejbmorina@gmail.com>
2025-04-21 23:10:03 +0200

    added life on the new login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  pin: string = '';
+  maxPinLength = 4;
+  dots: boolean[] = Array(this.maxPinLength).fill(false);
+
+  onPinButtonClick(number: string) {
+    if (this.pin.length < this.maxPinLength) {
+      this.pin += number;
+      this.updateDots();
+    }
+  }
+
+  private updateDots() {
+    this.dots = this.pin.split('').map(() => true);
+    while (this.dots.length < this.maxPinLength) {
+      this.dots.push(false);
+    }
+  }
+}
