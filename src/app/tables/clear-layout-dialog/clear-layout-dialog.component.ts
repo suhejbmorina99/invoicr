e8f4ce754a5a178bbf44055a2618527d54a4e073
suhejb <suhejbmorina@gmail.com>
2025-05-08 16:29:07 +0200

    working on clear layout modal

--- src/app/tables/clear-layout-dialog/clear-layout-dialog.component.ts
+++ src/app/tables/clear-layout-dialog/clear-layout-dialog.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common'
+
+
 
 @Component({
   selector: 'app-clear-layout-dialog',
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatDialogModule],
   templateUrl: './clear-layout-dialog.component.html',
   styleUrl: './clear-layout-dialog.component.scss'
 })
 export class ClearLayoutDialogComponent {
+  constructor(private dialogRef: MatDialogRef<ClearLayoutDialogComponent>) { }
 
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
 }
