fcd3a787cec27eef695b6331d93ab608414458e9
suhejb <suhejbmorina@gmail.com>
2025-05-08 15:32:29 +0200

    create new component to show the clear dialog

---
+++ src/app/tables/clear-layout-dialog/clear-layout-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clear-layout-dialog',
+  imports: [],
+  templateUrl: './clear-layout-dialog.component.html',
+  styleUrl: './clear-layout-dialog.component.scss'
+})
+export class ClearLayoutDialogComponent {
+
+}
